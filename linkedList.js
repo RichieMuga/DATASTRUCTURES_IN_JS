0bfd8927bf51f3194169b650a49e9b87f8677e54
Richie Mugambi <richiemugambi4@gmail.com>
2023-07-22 19:09:38 +0300

    feat: added file for linkedList

---
+++ linkedList.js
@@ -0,0 +1,8 @@
+// this is a datastructure that has a headend and a tailend
+// they contain nodes in that each node has a head and tail
+// they are null terminated, meaning that the tail at the end of the linked list is null
+
+// some pseudo code for a linked list
+
+// [apples,oranges, avocado] this is an array, lets convert it to a linked list
+//  apples --> oranges -->avodado -->null
