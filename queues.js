10fb6d5dfe06ccea938e14943e121fe3da6d9c53
Richie Mugambi <richiemugambi4@gmail.com>
2023-10-15 12:33:08 +0300

    feat: added queues part 1

---
+++ queues.js
@@ -0,0 +1,12 @@
+// This is a fifo datastructure, in that it is a first in first ou datastructure
+// imagine a queue for a bank, the first person to get there, gets to be served as the first one
+
+// These are the big(O) of the following operations in queue datastructure
+// lookup O(n), we usually dont do this on the datastructure
+// dequeue O(1), this is simply removing the first item on the list
+// enqueue O(1), this is the adding of an item to the end of the list
+// peek O(1), this is first item that is going to come out
+
+//Don't use arrays to create a queue.
+
+
