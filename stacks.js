6589d3bae61c0bfd472f8ca0f375ff9b7cd9a076
Richie Mugambi <richiemugambi4@gmail.com>
2023-10-15 12:34:50 +0300

    feat: added stacks part 1

---
+++ stacks.js
@@ -0,0 +1,7 @@
+// stacks are a lifo datastructure(last in first out)
+// imagine a stack of plates whereby one can add to the top and only remove the top plates
+// examples where stacks are used:
+// --stack overflow
+// --when browsing through pages, when you go back it accesses the page before the current page. ie. stacks
+// --when using undo and redo in your code editor or word document or etc.
+
